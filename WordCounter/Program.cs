88e5d845ec0471e6c5aa8e59e1310e6fd084efe8
Jack Dunning <JackStunning9001@gmail.com>
2020-03-06 11:01:17 -0800

    Add interaction logic

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -10,5 +10,12 @@ class Program
   static void Main()
   {
     myRepeatCounter = new RepeatCounter();
+
+    Console.WriteLine("Welcome to the WordCount App, it finds how many times a word occurs in a sentence.");
+    Console.WriteLine("Enter a word.");
+    string userWord = Console.ReadLine();
+    Console.WriteLine("Enter a sentence.");
+    string userSentence = Console.ReadLine();
+    Console.WriteLine(userWord + " " + userSentence);
   }
 }
