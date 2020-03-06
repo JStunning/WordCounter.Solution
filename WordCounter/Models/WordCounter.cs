476cc6b0cd95fe5472e802d888cb3a1b333281b0
Jack Dunning <JackStunning9001@gmail.com>
2020-03-06 08:42:04 -0800

    Write ReadMe with specs

---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordCounter.Models
+{
+  public class Counter
+  {
+    public int Count { get; set; }
+    public Counter()
+    {
+      Count = 0;
+    }
+  }
+}
